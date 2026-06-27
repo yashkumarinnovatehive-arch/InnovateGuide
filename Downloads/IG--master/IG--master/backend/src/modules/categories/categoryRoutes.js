b7212ac4925b4518d16c0627f64ad6326e7f27c5
MR.YashuHacker <100185004+Yashjihacker@users.noreply.github.com>
2026-06-27 16:22:59 +0530

    Clean up mistakenly merged Downloads directory

--- Downloads/IG--master/IG--master/backend/src/modules/categories/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCategories } = require('./categoryController');
-
-router.get('/', getCategories);
-
-module.exports = router;
--- Downloads/IG--master/IG--master/server/routes/categories.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCategories } = require('../controllers/categoryController');
-
-// GET /api/categories
-router.get('/', getCategories);
-
-module.exports = router;
